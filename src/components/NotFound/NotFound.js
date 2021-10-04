7a80e199ce1e2e3a199bb560044b933fd57ac4e6
rakibshakib <rakibshakib74@gmail.com>
2021-10-04 18:46:01 +0600

    update notfound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,13 +3,13 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div className='container mx-auto my-16'>
-            <h2 className="text-center text-8xl mt-12 font-bold text-red-500">
+            <h2 className="text-center  text-3xl md:text-4xl lg:text-8xl mt-12 font-bold text-red-500">
                 OoOps!
             </h2>
-            <h2 className="text-center text-5xl my-5 font-bold">
+            <h2 className="text-center text-xl md:text-2xl lg:text-5xl my-5 font-bold">
                 404 - Page Not Found 😫
             </h2>
-            <h2 className="text-center text-xl my-3 font-bold text-red-400">
+            <h2 className="text-center text-sm lg:text-xl my-3 font-bold text-red-400">
                 The Page You Were Looking For Couldn't Be Found.
             </h2>
         </div>
